bc64b556d6de989f98fcda563ff81830c64ec0fc
wolf0023 <wolf0023@ymt.bbiq.jp>
2024-09-19 00:13:26 +0900

    Update file paths and references in HTML files

--- js/script.js
+++
@@ -1,9 +0,0 @@
-// github pagesのため、/wolf0023server/を追加
-fetch("/wolf0023server/header.html")
-    .then((response) => response.text())
-    .then((data) => document.querySelector("main").insertAdjacentHTML('afterbegin', data))
-    .catch((error) => console.error('Error fetching header:', error));
-fetch("/wolf0023server/footer.html")
-    .then((response) => response.text())
-    .then((data) => document.querySelector("main").insertAdjacentHTML('afterend', data))
-    .catch((error) => console.error('Error fetching footer:', error));
