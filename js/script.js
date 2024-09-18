781388408f9ff0fc30b7034701b995c2be2a675f
wolf0023 <wolf0023@ymt.bbiq.jp>
2024-09-18 20:39:41 +0900

    Update file paths and references in 404 page

--- js/script.js
+++ js/script.js
@@ -1,8 +1,9 @@
-fetch("header.html")
+// github pagesのため、/wolf0023server/を追加
+fetch("/wolf0023server/header.html")
     .then((response) => response.text())
     .then((data) => document.querySelector("main").insertAdjacentHTML('afterbegin', data))
     .catch((error) => console.error('Error fetching header:', error));
-fetch("footer.html")
+fetch("/wolf0023server/footer.html")
     .then((response) => response.text())
     .then((data) => document.querySelector("main").insertAdjacentHTML('afterend', data))
     .catch((error) => console.error('Error fetching footer:', error));
