8b281e34db2982e86a04a3ba8b1f808732415201
wolf0023 <wolf0023@ymt.bbiq.jp>
2024-09-19 11:18:45 +0900

    Update file paths and references in script.js because of vulnerability

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 fetch("/wolf0023server/include/header.html")
     .then((response) => response.text())
-    .then((data) => document.querySelector("#header").innerHTML = data)
+    .then((data) => document.querySelector("#header").insertAdjacentHTML("afterbegin", data))
     .catch((error) => console.error('Error fetching header:', error));
 fetch("/wolf0023server/include/footer.html")
     .then((response) => response.text())
-    .then((data) => document.querySelector("#footer").innerHTML = data)
+    .then((data) => document.querySelector("#footer").insertAdjacentHTML("afterbegin", data))
     .catch((error) => console.error('Error fetching footer:', error));
 fetch("/wolf0023server/include/nav.html")
     .then((response) => response.text())
-    .then((data) => document.querySelector("#nav").innerHTML = data)
+    .then((data) => document.querySelector("#nav").insertAdjacentHTML("afterbegin", data))
     .catch((error) => console.error('Error fetching footer:', error));
