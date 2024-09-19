69bd0972de0e6b3c7895f63405acf80c7b0af032
wolf0023 <wolf0023@ymt.bbiq.jp>
2024-09-19 11:14:00 +0900

    Update file paths and references in HTML, CSS, and JavaScript files

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
 fetch("/wolf0023server/include/header.html")
     .then((response) => response.text())
-    .then((data) => document.querySelector("main").insertAdjacentHTML('afterbegin', data))
+    .then((data) => document.querySelector("#header").innerHTML = data)
     .catch((error) => console.error('Error fetching header:', error));
 fetch("/wolf0023server/include/footer.html")
     .then((response) => response.text())
-    .then((data) => document.querySelector("main").insertAdjacentHTML('afterend', data))
+    .then((data) => document.querySelector("#footer").innerHTML = data)
+    .catch((error) => console.error('Error fetching footer:', error));
+fetch("/wolf0023server/include/nav.html")
+    .then((response) => response.text())
+    .then((data) => document.querySelector("#nav").innerHTML = data)
     .catch((error) => console.error('Error fetching footer:', error));
